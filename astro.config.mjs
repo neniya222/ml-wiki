f3bfdff09d2e983a258fb6caa327a9b99b7e4d8d
neniya222 <neniya222@gmail.com>
2026-03-29 11:53:24 +0900

    Add vein miner feature to configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,6 +36,7 @@ export default defineConfig({
 						{ label: '🚀 テレポート機能', link: '/features/tp' },
 						{ label: '🛡️ 土地保護', link: '/features/claims' },
 						{ label: '📦️ チェスト・ドアの保護', link: '/features/cdhogo' },
+						{ label: '⛏️ 一括破壊', link: '/features/veinminer' },
 						{ label: '🏆 スキル機能', link: '/features/skills' },
 						{ label: '🥚 エッグキャッチャー', link: '/features/eggc' },
 					],
